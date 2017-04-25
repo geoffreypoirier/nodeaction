16e9dfc88e30d896138b5a7367bbd30ea9b797c8
Geoffrey Poirier <geoffreypoirier@gmail.com>
2017-04-25 16:15:05 -0600

    add CORS

--- src/app.js
+++ src/app.js
@@ -12,6 +12,16 @@ let app     = express();
 let subpath = express();
 
 
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-XSRF-TOKEN, Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
+  next();
+});
+
+
+
 // use bodyParser for the rendering engine
 //app.use(bodyParser.urlencoded({'extended': 'false'}));
 
